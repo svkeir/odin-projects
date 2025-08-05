7ebb24c1772a807e3053db7e6203328e786bea44
skeir <169955861+svkeir@users.noreply.github.com>
2025-08-05 15:18:04 -0300

    edit random number gen to jokenpo selector

--- jokenpo.js
+++ jokenpo.js
@@ -1,14 +1,16 @@
-function getRandomInt(){
-    return Math.floor(Math.random() * 3);
+function getComputerChoice(){
+    const randomInt = Math.floor(Math.random() * 3)
+
+    if (randomInt === 0) {
+    return "rock"; }
+    else if (randomInt === 1){
+    return "paper"; } 
+    else {
+    return "scissors";}
     
 
 }
 
-console.log(getRandomInt())
-
-
-
-
-
+console.log(getComputerChoice())
 
 
